156a1cbf966910c1ee5b8fe3219a22fc38ab309d
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-08-12 17:49:50 +0530

    Load all images on changing into the menu tab with random prices, tweak styles of foodDiv

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,17 @@
 import burger from "../img/menu/burger.jpg";
+import burritos from "../img/menu/burritos.jpeg";
+import cake from "../img/menu/cake.jpg";
+import cereals from "../img/menu/cereals.jpg";
+import croissant from "../img/menu/croissant.jpg";
+import italian from "../img/menu/italian.jpg";
+import korean from "../img/menu/korean.jpg";
+import omlette from "../img/menu/omlette.jpg";
+import pakora from "../img/menu/pakora.jpg";
+import pancakes from "../img/menu/pancakes.jpg";
+import pizza from "../img/menu/pizza.jpg";
+import salads from "../img/menu/salads.jpg";
+import tempura from "../img/menu/tempura.jpg";
+import thai from "../img/menu/thai.jpg";
 
 export default function () {
   const content = document.querySelector("#content");
@@ -11,16 +24,48 @@ export default function () {
 
   menuDiv.append(menuHeading);
 
-  const foodItemsDiv = document.createElement("div");
+  let foodItemsDiv = document.createElement("div");
   foodItemsDiv.id = "foodItems";
 
-  for (let i = 0; i < 8; i++) {
-    const foodDiv = createFoodDiv("Burger", burger, 12);
-    foodItemsDiv.append(foodDiv);
-  }
+  foodItemsDiv = loadMenu(foodItemsDiv);
 
   menuDiv.append(foodItemsDiv);
   content.append(menuDiv);
+}
+
+function loadMenu(foodItemsDiv) {
+  const dishes = [
+    { name: "Burger", src: burger },
+    { name: "Burritos", src: burritos },
+    { name: "Cake", src: cake },
+    { name: "Cereals", src: cereals },
+    { name: "Crosissant", src: croissant },
+    { name: "Italian", src: italian },
+    { name: "Korean", src: korean },
+    { name: "Omlette", src: omlette },
+    { name: "Pakora", src: pakora },
+    { name: "Pancakes", src: pancakes },
+    { name: "Pizza", src: pizza },
+    { name: "Salads", src: salads },
+    { name: "Tempura", src: tempura },
+    { name: "Thai", src: thai },
+  ];
+
+  for (let dish of dishes) {
+    const foodDiv = createFoodDiv(dish.name, dish.src, createRandNum());
+    foodItemsDiv.append(foodDiv);
+  }
+
+  return foodItemsDiv;
+
+  // for (let i = 0; i < 8; i++) {
+  //   const foodDiv = createFoodDiv("Burger", burger, 12);
+  //   foodItemsDiv.append(foodDiv);
+  // }
+}
+
+function createRandNum() {
+  return Math.round(Math.random() * 20 + 10);
 }
 
 function createFoodDiv(fName, imageSrc, fPrice) {
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/scripts/index.js",
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
